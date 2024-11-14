83aa1935f1fda7e11fe671791322b00b638478ae
0woodev <0woodev@gmail.com>
2024-11-14 22:08:17 +0900

    INIT - 인증서 만료에 대해서 알림을 보내는 서비스 개발 시작

---
+++ src/main/java/com/woodev/noticerti/NoticertiApplication.java
@@ -0,0 +1,13 @@
+package com.woodev.noticerti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticertiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticertiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woodev/noticerti/NoticertiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woodev.noticerti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticertiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
