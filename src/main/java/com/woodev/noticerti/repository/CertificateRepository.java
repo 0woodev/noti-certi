1a9dfe4acf3b8987119a9e8482e985b34080e35a
0woodev <0woodev@gmail.com>
2024-11-15 16:52:37 +0900

    TDD - DB 에서 Certificate 조회 테스트 코드 작성

--- src/main/java/com/woodev/noticerti/repository/CertificateRepository.java
+++ src/main/java/com/woodev/noticerti/repository/CertificateRepository.java
@@ -3,5 +3,9 @@ package com.woodev.noticerti.repository;
 import com.woodev.noticerti.model.Certificate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CertificateRepository extends JpaRepository<Certificate, Long> {
+    Optional<Certificate> findByIssuingCAAndSerialNumber(String issuingCA, String serialNumber);
+
 }
--- src/test/java/com/woodev/noticerti/service/impl/CertificateServiceImplTest.java
+++ src/test/java/com/woodev/noticerti/service/impl/CertificateServiceImplTest.java
@@ -1,19 +1,37 @@
 package com.woodev.noticerti.service.impl;
 
 import com.woodev.noticerti.dto.CertificateInfoDTO;
-import com.woodev.noticerti.service.CertificateService;
+import com.woodev.noticerti.model.Certificate;
+import com.woodev.noticerti.repository.CertificateRepository;
 import com.woodev.noticerti.util.URLBuilder;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.net.URL;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
+
+@ExtendWith(MockitoExtension.class)
 class CertificateServiceImplTest {
-    private final CertificateService certificateService = new CertificateServiceImpl();
+
+    @InjectMocks
+    private CertificateServiceImpl certificateService;
+
+    @Mock
+    private CertificateRepository certificateRepository;
+
 
     @Test
     void getCertificateFromServer() {
@@ -73,11 +91,41 @@ class CertificateServiceImplTest {
 
     @Test
     void getCertificateFromDB() {
-        // given
+        try {
+            // given
+            Certificate cert = Certificate.builder()
+                    .id(1L)
+                    .validFrom(Instant.now().minusSeconds(86400))
+                    .validTo(Instant.now().plusSeconds(31536000))
+                    .commonName("www.0woodev.com")
+                    .serialNumber("abcdefg")
+                    .issuingCA("0woodevCA")
+                    .organization("0woodev")
+                    .build();
 
-        // when
+            // repository 에서 가져오는 것이므로 null 이 아닌 cert 객체를 반환하도록 설정
+            when(certificateRepository.findByIssuingCAAndSerialNumber(any(String.class), any(String.class)))
+                    .thenReturn(Optional.of(cert));
 
-        // then
+            URL url = URLBuilder.getHttps("www.0woodev.com", 443);
+
+            // when
+            Certificate actual = certificateService.getCertificateFromDB(url);
+
+            // then
+            assertThat(actual).isNotNull();
+            assertThat(actual.getId()).isEqualTo(cert.getId());
+            verify(certificateRepository, times(1))
+                    .findByIssuingCAAndSerialNumber(any(String.class), any(String.class));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+
+
+
 
     }
 
